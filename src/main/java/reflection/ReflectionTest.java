1e276761a2a3a4b8f1615cd050535935442f6026
7865754654 <161445321+7865754654@users.noreply.github.com>
2026-03-15 12:21:56 +0300

    Add sout

--- src/main/java/reflection/ReflectionTest.java
+++ src/main/java/reflection/ReflectionTest.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 public class ReflectionTest {
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         System.out.println("Проверка гит пул");
+        System.out.println("I'm so sad:(((((((");
         User user = new User("Lavanda", 22);
         Class<User> userClass = User.class;
         System.out.println(userClass.getSuperclass());
